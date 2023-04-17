9c6799d2c768eb40c09d144bce91fed2340e0604
marcosmrk1 <marcospl.2990@gmail.com>
2023-04-17 12:03:29 -0300

    Ajustes Nos Nomes feitos

---
+++ src/auth.js
@@ -0,0 +1,7 @@
+// export const  funcaologin=() => {
+//   function Protected({ isSignedIn, children }) {
+//     if (!isSignedIn) {
+//         return <Navigate to="/" replace />
+//     }
+//     return children
+// }}
